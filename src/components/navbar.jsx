7dadc794f2949dee7cf76f8e6fbe904c994df8c2
tany0812 <chaitanyachangala8@gmail.com>
2025-02-20 10:52:34 -0500

    finished nav bar for both mobile and webpage

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,25 @@ import styles from './NavBar.module.css';
 import {getImageUrl} from "../utils";
 
 function NavBar(){
+    const [menuOpen,setMenuOpen] = useState('false');
     return (
         <nav className={styles.navbar}>
             <a href="#portfolio" className={styles.title}>Portfolio</a>
             <div className={styles.menu}>
-            <img className={styles.menuBtn} src='../../public/assets/nav/menuIcon.png' alt='menu-button'></img>
-                <ul className={styles.menuItems}>
+                <img className={styles.menuBtn} src={ 
+                    menuOpen?
+                    getImageUrl('nav/closeIcon.png'):
+                    getImageUrl('nav/menuIcon.png')
+                    } 
+                    alt='menu-button' 
+                    onClick ={()=>{
+                        setMenuOpen(!menuOpen)}
+                        }
+                />
+                <ul 
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick ={()=>setMenuOpen(false)}
+                >
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#projects">Projects</a></li>
